3737da873381bd0c005a70465ab183745c95d684
Mateus Guizelini <dev.guizelini@hotmail.com>
2025-07-07 10:06:12 -0300

    Categoria controller, model, factory, migrate, seeder

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+
+class CategoriaController extends Controller
+{
+    public function index()
+    {
+        $categorias = Categoria::all();
+        return view('categorias.index', compact('categorias'));
+    }
+
+    public function show($id)
+    {
+        $categoria = Categoria::findOrFail($id);
+        return view('categorias.show', compact('categoria'));
+    }
+
+    public function create()
+    {
+        return view('categorias.create');
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'nome' => 'required|string|max:255|unique:categorias,nome',
+        ]);
+        Categoria::create($data);
+        return redirect()->route('categorias.index')->with('success', 'Categoria criada com sucesso!');
+    }
+
+    public function edit($id)
+    {
+        $categoria = Categoria::findOrFail($id);
+        return view('categorias.edit', compact('categoria'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $categoria = Categoria::findOrFail($id);
+        $data = $request->validate([
+            'nome' => 'required|string|max:255|unique:categorias,nome,except,id',
+        ]);
+        $categoria->update($data);
+        return redirect()->route('categorias.index')->with('success', 'Categoria atualizada com sucesso!');
+    }
+
+    public function destroy($id)
+    {
+        $categoria = Categoria::findOrFail($id);
+        $categoria->delete();
+        return redirect()->route('categorias.index')->with('success', 'Categoria deletada com sucesso!');
+    }
+} 
